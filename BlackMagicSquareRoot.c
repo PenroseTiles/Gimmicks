7a32cd4ff48e95c6fa54688dcd70372684512765
Anurag Kashyap <53425257+PenroseTiles@users.noreply.github.com>
2021-04-21 02:34:12 -0400

    Create BlackMagicSquareRoot.c

---
+++ BlackMagicSquareRoot.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+
+/*Credit to the developers of Quake 3, this code is not mine*/
+float Q_rsqrt( float number )
+{
+	long i;
+	float x2, y;
+	const float threehalfs = 1.5F;
+
+	x2 = number * 0.5F;
+	y  = number;
+	i  = * ( long * ) &y;                       // evil floating point bit level hacking
+	i  = 0x5f3759df - ( i >> 1 );               // what the fuck? 
+	y  = * ( float * ) &i;
+	y  = y * ( threehalfs - ( x2 * y * y ) );   // 1st iteration
+	y  = y * ( threehalfs - ( x2 * y * y ) );   // 2nd iteration
+
+	return y;
+}
+
+
+int main(){
+
+  printf("%f", 1.0/Q_rsqrt(25.0));
+}
